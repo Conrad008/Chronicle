634f4079ae92982fd32423f8658f75b94cae1e2e
Conrad008 <kipngeno7180@gmail.com>
2026-09-01 23:47:24 +0300

    feat: add user registration view

--- photo_gallery/views.py
+++ photo_gallery/views.py
@@ -6,3 +6,14 @@ from django.contrib.auth import update_session_auth_hash
 from django.db.models import Q
 from .models import Photo, Tag, Profile
 
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            Profile.objects.create(user=user)
+            login(request, user)
+            return redirect('photo_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
