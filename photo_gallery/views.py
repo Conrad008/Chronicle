a2cf2348637175d7e0e89cd92d000bd374f67eda
Conrad008 <kipngeno7180@gmail.com>
2026-09-01 23:37:06 +0300

    chore: import auth, shortcut, and model dependencies

--- photo_gallery/views.py
+++ photo_gallery/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from django.db.models import Q
+from .models import Photo, Tag, Profile
 
-# Create your views here.
