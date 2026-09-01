d5e9584a9985959897d83c52f2672f7dcb10a682
Conrad008 <kipngeno7180@gmail.com>
2026-09-01 23:16:20 +0300

    feat: create Tag model with  name field

--- photo_gallery/models.py
+++ photo_gallery/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
