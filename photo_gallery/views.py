4e8af8f4f811f57bc86aa5fe89146e5b065b8002
Conrad008 <kipngeno7180@gmail.com>
2026-09-03 00:14:05 +0300

    fix: replace UserCreationForm with CustomUserCreationForm in register_view

--- photo_gallery/views.py
+++ photo_gallery/views.py
@@ -5,17 +5,18 @@ from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.contrib.auth import update_session_auth_hash
 from django.db.models import Q
 from .models import Photo, Tag, Profile
+from .forms import CustomUserCreationForm
 
 def register_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             Profile.objects.create(user=user)
             login(request, user)
             return redirect('photo_list')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'register.html', {'form': form})
 
 def photo_list(request):
