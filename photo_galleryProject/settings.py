a03a29396b36637521e1a485bf1aa389173baade
Conrad008 <kipngeno7180@gmail.com>
2026-09-05 01:13:26 +0300

    fix: update DEBUG setting for production environment

--- photo_galleryProject/settings.py
+++ photo_galleryProject/settings.py
@@ -30,7 +30,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+DEBUG = os.environ.get('RENDER') is None
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com']
 
