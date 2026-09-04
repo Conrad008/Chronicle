8c63f0527175016117c1d04eab5cddaf1d801c5e
Conrad008 <kipngeno7180@gmail.com>
2026-09-05 02:17:08 +0300

    fix: reorder commands in build script for static files handling

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
 
+rm -rf staticfiles
+
 python manage.py collectstatic --no-input
 python manage.py migrate
-
